589150812d130bf9698dd0dad01558fae91d1523
Jefferson Abreu <nabucodonosor310@gmail.com>
2020-04-22 22:16:24 -0300

    criado primeiro componente

--- src/test/app.spec.js
+++ src/test/app.spec.js
@@ -3,10 +3,21 @@ import "angular-mocks";
 import "./../app";
 
 describe("App module", () => {
+  var $compile;
+  var $rootScope;
+
   beforeEach(() => {
     angular.mock.module("app");
   });
-  it("teste inicial: should show message pending", () => {
-    pending("karma está funcional");
+
+  beforeEach(inject((_$compile_, _$rootScope_) => {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+  }));
+
+  it("teste inicial: should show message 'Olá mundo' in page'", () => {
+    var elementApp = $compile("<app></app>")($rootScope);
+    $rootScope.$digest();
+    expect(elementApp.html()).toContain("Olá mundo");
   });
 });
